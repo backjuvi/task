564cc290433ccc548c37dad5141b16af0747d428
backjuvi <136236403+backjuvi@users.noreply.github.com>
2023-06-11 16:15:30 +0530

    Add files via upload

---
+++ dash.php
@@ -0,0 +1,18 @@
+<?php
+
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard - Client area</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="form">
+        <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
+        <p>Welcome To This Page.</p>
+          </div>
+</body>
+</html>
